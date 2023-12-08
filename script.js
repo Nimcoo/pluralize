fac30fc2b90917334afcb5cbc1ecdd9e66daf54c
NIMCOO <nimco77001@gmail.com>
2023-12-08 08:54:33 +0000

    Done Exercise

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // Good Luck 💪🏾
+const names = ["cat","dog","cat","mouse"];
+const pluralize= names.map((item)=>{
+    return item[0].toUpperCase() + item.slice(1).toLowerCase();
+
+});
+
+console.log(pluralize);
+
+
+
+
+
+
+pluralize(["car", "car", "car"]) 
